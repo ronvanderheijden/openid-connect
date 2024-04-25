ac8847c0c771c9bd771d9d5ef36abc917f9ab77c
ronvanderheijden <r.heijden@live.nl>
2024-04-25 20:54:43 +0200

    Added declare strict types

--- src/Laravel/DiscoveryController.php
+++ src/Laravel/DiscoveryController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace OpenIDConnect\Laravel;
 
 use Illuminate\Http\Request;
--- src/Laravel/routes/web.php
+++ src/Laravel/routes/web.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 if (config('openid.routes.discovery', true)) {
     Route::get('/oauth/jwks', \OpenIDConnect\Laravel\JwksController::class)->name('openid.jwks');
 }
